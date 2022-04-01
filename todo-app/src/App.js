a8d8b6701475010c7cef158fc079613270048416
JIHy0228 <vkdnjtjsej07@naver.com>
2022-04-01 13:53:21 +0900

    todo완성본

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,13 +1,69 @@
-import './App.css';
+import React, { useState, useRef, useCallback, useReducer } from 'react';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
+function createBulkTodos() {
+  const array = [];
+  for (let i = 0; i < 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
+
+const reducer = (todos, action) => {
+  switch (action.type) {
+    case 'INSERT': //새로 추가
+      return todos.concat(action.todo);
+    case 'REMOVE':
+      return todos.filter((todo) => todo.id !== action.id);
+    case 'TOGGLE':
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+};
+
 function App() {
+  const [todos, dispatch] = useReducer(reducer, undefined, createBulkTodos);
+
+  // 고윳값으로 사용될 id
+  // ref를 사용하여 변수 담기
+  const nextId = useRef(2501);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      dispatch({ type: 'INSERT', todo });
+      nextId.current += 1;
+    },
+    [todos],
+  );
+
+  // 삭제 구현 onRemove
+  const onRemove = useCallback((id) => {
+    dispatch({ type: 'REMOVE', id });
+  }, []);
+
+  // 수정 구현 onToggle
+  const onToggle = useCallback((id) => {
+    dispatch({ type: 'TOGGLE', id });
+  }, []);
+
   return (
     <TodoTemplate>
-      <TodoInsert />
-      <TodoList />
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
 }
