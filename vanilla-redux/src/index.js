cc9fc640aaf9f7f200e75b139159f698ff110d5d
JIHy0228 <vkdnjtjsej07@naver.com>
2022-07-01 16:35:56 +0900

    vanillaJS-Redux TodoApp

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,50 +1,85 @@
-import { legacy_createStore } from "redux";
+import { createStore } from "redux";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-number.innerText = 0;
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-const ADD = "ADD";
-const MINUS = "MINUS";
+/*
+    state는 single source of truth이며, read-only이다.
+    store를 수정할 수 있는 유일한 방법은 action을 보내는 방법뿐이다.
+    state를 mutate하지 말아야한다.
+    mutating state하는 대신에 new state objects를 리턴해야 한다.
+    ex) state.push(action.text) => state에 푸쉬를 하는 것이 아니라 새로운 배열을 만들어 전달.
 
-//   reducer (state, action)
-/*   action 형식 : object
-     action 필수 요소 : type
+
+    ...state => state array안의 모든 content
 */
-const countModifier = (count = 0, action) => {
+
+const addToDo = (text) => {
+  return {
+    type: ADD_TODO,
+    text,
+  };
+};
+
+const deleteToDo = (id) => {
+  return {
+    type: DELETE_TODO,
+    id,
+  };
+};
+
+const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
+    case ADD_TODO:
+      return [...state, { text: action.text, id: Date.now() }];
+    case DELETE_TODO:
+      return state.filter((toDo) => toDo.id !== action.id);
     default:
-      return count;
+      return state;
   }
 };
 
-// store
-// counterStore data 저장소
-const countStore = legacy_createStore(countModifier);
+const store = createStore(reducer);
 
-const onChange = () => {
-  number.innerText = countStore.getState();
+store.subscribe(() => console.log(store.getState()));
+
+const dispatchAddToDo = (text) => {
+  store.dispatch(addToDo(text));
 };
 
-countStore.subscribe(onChange);
-
-/*  action 형식 : object
-    action 필수 요소 : type
-    dispatch : store의 내장 함수 => action을 발생시키는 것
-*/
-const handleAdd = () => {
-  countStore.dispatch({ type: ADD });
+const dispatchDeleteTodo = (e) => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteToDo(id));
+  console.log(e.target.parentNode.id);
 };
 
-const handleMinus = () => {
-  countStore.dispatch({ type: MINUS });
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach((toDo) => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchDeleteTodo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
 };
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+store.subscribe(paintToDos);
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  dispatchAddToDo(toDo);
+};
+
+form.addEventListener("submit", onSubmit);
