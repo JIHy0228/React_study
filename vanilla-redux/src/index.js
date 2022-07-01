1c32ce26ae7dad893294a10b3baed48816cfb0da
JIHy0228 <vkdnjtjsej07@naver.com>
2022-07-01 18:01:00 +0900

    노마드코더 #REACT REDUX (3.2)

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,85 +1,12 @@
-import { createStore } from "redux";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import { Provider } from "react-redux";
+import store from "./store";
 
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const ul = document.querySelector("ul");
-
-const ADD_TODO = "ADD_TODO";
-const DELETE_TODO = "DELETE_TODO";
-
-/*
-    state는 single source of truth이며, read-only이다.
-    store를 수정할 수 있는 유일한 방법은 action을 보내는 방법뿐이다.
-    state를 mutate하지 말아야한다.
-    mutating state하는 대신에 new state objects를 리턴해야 한다.
-    ex) state.push(action.text) => state에 푸쉬를 하는 것이 아니라 새로운 배열을 만들어 전달.
-
-
-    ...state => state array안의 모든 content
-*/
-
-const addToDo = (text) => {
-  return {
-    type: ADD_TODO,
-    text,
-  };
-};
-
-const deleteToDo = (id) => {
-  return {
-    type: DELETE_TODO,
-    id,
-  };
-};
-
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      return [...state, { text: action.text, id: Date.now() }];
-    case DELETE_TODO:
-      return state.filter((toDo) => toDo.id !== action.id);
-    default:
-      return state;
-  }
-};
-
-const store = createStore(reducer);
-
-store.subscribe(() => console.log(store.getState()));
-
-const dispatchAddToDo = (text) => {
-  store.dispatch(addToDo(text));
-};
-
-const dispatchDeleteTodo = (e) => {
-  const id = parseInt(e.target.parentNode.id);
-  store.dispatch(deleteToDo(id));
-  console.log(e.target.parentNode.id);
-};
-
-const paintToDos = () => {
-  const toDos = store.getState();
-  ul.innerHTML = "";
-  toDos.forEach((toDo) => {
-    const li = document.createElement("li");
-    const btn = document.createElement("button");
-    btn.innerText = "DEL";
-    btn.addEventListener("click", dispatchDeleteTodo);
-    li.id = toDo.id;
-    li.innerText = toDo.text;
-
-    li.appendChild(btn);
-    ul.appendChild(li);
-  });
-};
-
-store.subscribe(paintToDos);
-
-const onSubmit = (e) => {
-  e.preventDefault();
-  const toDo = input.value;
-  input.value = "";
-  dispatchAddToDo(toDo);
-};
-
-form.addEventListener("submit", onSubmit);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
---
+++ vanilla-redux/src/routes/Home.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+
+function Home(props) {
+  console.log(props);
+  const [text, setText] = useState("");
+  function onChange(e) {
+    setText(e.target.value);
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+    setText("");
+  }
+
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>Add</button>
+      </form>
+      <ul></ul>
+    </>
+  );
+}
+
+function getCurrentState(state, ownProps) {
+  return { state };
+}
+
+export default connect(getCurrentState)(Home);
+
+/*
+    Connect : 2개의 arguments (state, dispatch)
+ */
